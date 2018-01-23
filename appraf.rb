71deb4c314580841f278798ecd66be4c78cf36fb
bobylegros <rafcapdeville@gmail.com>
2018-01-23 23:28:55 +0100

    suite2

--- appraf.rb
+++ appraf.rb
@@ -1,12 +1,26 @@
 require_relative 'projet'
 require_relative 'scrap_mairies'
+
 def get_rows_from_items(items) # boîboîte (fonction) "utilitaire" (reçoit un truc ave des hash ... mais s'assure qu'on ne retourne qu'un array = une ligne avec n colonnes = utilisable par un tableaur !)
   items.map { |item| [item[0]] + item[1].values } # je transforme un tableau contenant un hash en un "simple" tableau = plus de clés (juste les valeurs = les colonnes) => 1 ligne d'infos (1 colonne = 1 info, ex.: nom de la ville, email, etc.) = 1 ville ... n lignes = n villes ... c'est une TABLE (= le MÊME nombre de colonnes à CHAQUE ligne) !!!
 end
+
 destinataires = ['rafcapdeville@gmail.com', 'sebdoe75@gmail.com', 'jean25pierre2@gmail.com', 'whois.marvin.42@protonmail.com']
 
 
 $base_url = "http://annuaire-des-mairies.com/"
-p get_emails_from_all_towns_in_dpt("val-d-oise.html")
+villes_et_mairies = get_emails_from_all_towns_in_dpt("val-d-oise.html")
+p villes_et_mairies
+villes_destinataires = get_rows_from_items(villes_et_emails)
+p villes_destinataires
+
+
+
+
+
+
+
+
+
 
 # send_mails(destinataires)
